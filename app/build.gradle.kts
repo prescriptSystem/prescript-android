53eac991e6863b54dbeae062129eab97759683d6
Victor <prescript.sac@gmail.com>
2024-08-09 16:28:26 -0300

    Correção do projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
 
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
